4642e8d5aa30d422380e3d5f37eef6c0bbb786f2
PeizuLi5 <peizu.li@sjsu.edu>
2023-10-05 16:24:36 -0700

    frontend update

--- frontend/react_app/src/App.js
+++ frontend/react_app/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css';
+
+import Homepage from './Components/Homepage/Homepage.jsx'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <div className='content'>
+          <Routes>
+            <Route path="/" exact element={<Homepage />}/>
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/react_app/src/Components/Homepage/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Homepage.css"
+
+const Homepage = () => {
+    return (
+        <div class='title'>
+            <h1>Hello World! This is Homepage!</h1>
+        </div>
+    );
+};
+
+export default Homepage;
