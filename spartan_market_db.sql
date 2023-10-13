766d696ae584519fd132f8bf30bbb89e7bc0d7e5
SamZhao <samzhao0528@gmail.com>
2023-10-13 00:47:36 -0700

    updated sql file fixed few errors

--- spartan_market_db.sql
+++ spartan_market_db.sql
@@ -4,6 +4,8 @@ USE spartan_market;
 
 CREATE TABLE Users(
 	user_id INT PRIMARY KEY,
+    username VARCHAR(200) UNIQUE NOT NULL,
+    user_password VARCHAR(255) COLLATE utf8_unicode_ci NOT NULL,
     f_name INT NOT NULL,
     l_name INT NOT NULL,
     email VARCHAR(200) UNIQUE NOT NULL,
@@ -24,6 +26,7 @@ CREATE TABLE Service(
  
  CREATE TABLE Item(
 	item_id INT PRIMARY KEY,
+    seller_id INT NOT NULL,
     item_name VARCHAR(50) NOT NULL,
     item_type VARCHAR(20) NOT NULL,
     item_price FLOAT NOT NULL,
@@ -33,15 +36,16 @@ CREATE TABLE Service(
     item_status VARCHAR(10) NOT NULL,
     post_datetime TIMESTAMP NOT NULL,
     item_description VARCHAR(1000) NOT NULL,
-    course_tab VARCHAR(20)
+    course_tab VARCHAR(20),
+    FOREIGN KEY (seller_id) REFERENCES Users(user_id)
  );
  
  CREATE TABLE InterestQueue(
 	queue_id INT PRIMARY KEY,
     queue_position INT NOT NULL,
-    user_id INT NOT NULL,
+    buyer_id INT NOT NULL,
     item_id INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (buyer_id) REFERENCES Users(user_id),
     FOREIGN KEY (item_id) REFERENCES Item(item_id)
  );
  
