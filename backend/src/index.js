d5f59c08b517ab4df6fe4031b97a8dd4ec738774
SamZhao <samzhao0528@gmail.com>
2023-11-30 16:05:06 -0800

    added registration backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,39 +1,38 @@
-const express = require('express');
-const mysql = require('mysql');
-const cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+require("dotenv").config();
+const db = require("./config/db");
 
-const accounts = require('./routes/accounts')
+const accounts = require("./routes/accounts");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use("/accounts", accounts)
+app.use("/accounts", accounts);
 
-const db = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    port: 3306,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE
-})
+// const db = mysql.createConnection({
+//     host: process.env.DATABASE_HOST,
+//     port: 3306,
+//     user: process.env.DATABASE_USER,
+//     password: process.env.DATABASE_PASSWORD,
+//     database: process.env.DATABASE
+// })
 
-db.connect(function(err) {
-    if (err) throw err;
-    app.get('/', (req, res) => {
-        res.send("Connected to Database.");
-    })
-    console.log("Connected to Database");
-});
+// db.connect(function(err) {
+//     if (err) throw err;
+//     app.get('/', (req, res) => {
+//         res.send("Connected to Database.");
+//     })
+//     console.log("Connected to Database");
+// });
 
 const PORT = 3001;
 
 app.listen(PORT, (err) => {
-    if (err)
-        console.log("Error, server cannot start", err)
-    else
-        console.log("Server Connected, and running in PORT: " + PORT)
-})
+  if (err) console.log("Error, server cannot start", err);
+  else console.log("Server Connected, and running in PORT: " + PORT);
+});
 
 module.exports = { app, db };
