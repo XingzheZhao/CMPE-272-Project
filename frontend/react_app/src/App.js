3fd1c0d15d2f8994156b979c72f570e6ff5d3757
SamZhao <samzhao0528@gmail.com>
2023-12-03 16:08:44 -0800

    register

--- frontend/react_app/src/App.js
+++ frontend/react_app/src/App.js
@@ -1,35 +1,37 @@
-import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 
-import Auth from './Components/Auth/Auth';
-import ForgetPassword from './Components/ForgetPassword/ForgetPassword';
-import Homepage from './Components/Homepage/Homepage';
-import ResetPassword from './Components/ResetPassword/ResetPassword';
-import Navbar from './Components/Navbar/Navbar';
-import Error from './Components/Error/Error';
-import SearchItem from './Components/SearchItem/SearchItem';
-import Item from './Components/Item/Item';
-import Login from './Components/Login/Login';
-import EditItem from './Components/EditItem/EditItem';
+import Auth from "./Components/Auth/Auth";
+import ForgetPassword from "./Components/ForgetPassword/ForgetPassword";
+import Homepage from "./Components/Homepage/Homepage";
+import ResetPassword from "./Components/ResetPassword/ResetPassword";
+import Navbar from "./Components/Navbar/Navbar";
+import Error from "./Components/Error/Error";
+import SearchItem from "./Components/SearchItem/SearchItem";
+import Item from "./Components/Item/Item";
+import Login from "./Components/Login/Login";
+import EditItem from "./Components/EditItem/EditItem";
+import Register from "./Components/Register/Register";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-      <Navbar/>
-        <div className='content'>
+        <Navbar />
+        <div className="content">
           <Routes>
-            <Route path="/" exact element={<Homepage />}/>
-            <Route path='/items' exact element={<Homepage />}/>
-            <Route path="/auth/:id" exact element={<Auth />}/>
-            <Route path="/login" exact element={< Login/>}/>
-            <Route path="/forget-password" exact element={< ForgetPassword/>}/>
-            <Route path="/reset-password" exact element={<ResetPassword/>}/>
-            <Route path="/item/:status/:id/edit" exact element={<EditItem/>}/>
-            <Route path="/item/:status/:id" exact element={<Item/>}/>
-            <Route path="/items/:text" exact element={<SearchItem/>}/>
-            <Route path="*" element={<Error/>}/>
+            <Route path="/" exact element={<Homepage />} />
+            <Route path="/items" exact element={<Homepage />} />
+            <Route path="/auth/:id" exact element={<Auth />} />
+            <Route path="/login" exact element={<Login />} />
+            <Route path="/forget-password" exact element={<ForgetPassword />} />
+            <Route path="/reset-password" exact element={<ResetPassword />} />
+            <Route path="/item/:status/:id/edit" exact element={<EditItem />} />
+            <Route path="/item/:status/:id" exact element={<Item />} />
+            <Route path="/items/:text" exact element={<SearchItem />} />
+            <Route path="/register" exact element={<Register />} />
+            <Route path="*" element={<Error />} />
           </Routes>
         </div>
       </div>
---
+++ frontend/react_app/src/Components/Register/Register.jsx
@@ -0,0 +1,177 @@
+import React from "react";
+import axios from "axios";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Button, TextField } from "@mui/material";
+import "./Register.css";
+
+const Register = () => {
+  const navigate = useNavigate();
+
+  const [data, setData] = useState({
+    username: "",
+    userPassword: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+  });
+
+  const [isValidEmail, setIsValidEmail] = useState(true);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const validateEmail = (email) => {
+    const regex = /@sjsu\.edu$/;
+
+    const isValid = regex.test(email);
+
+    console.log(`isvalid: ${isValid}`);
+
+    console.setIsValidEmail(isValid);
+  };
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setIsLoading(true);
+
+    // check email validation first
+
+    if (!isValidEmail) {
+      setError("You must use your SJSU EMail Address!");
+    } else {
+      try {
+        const url = "http://localhost:3001/accounts/register";
+
+        const { data: res } = await axios.post(url, data);
+
+        navigate("/login");
+
+        window.AbortController.reloard();
+      } catch (err) {
+        if (
+          err.response &&
+          err.response.status >= 400 &&
+          err.response.status <= 500
+        ) {
+          setError(err.response.data.message);
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    }
+  };
+
+  if (isLoading) {
+    return <div className="loading-spinner">Loading</div>;
+  } else {
+    return (
+      <div className="register_container">
+        <h1 className="header">Join Spartan Market</h1>
+
+        <form onSubmit={handleSubmit}>
+          <div className="register-field">
+            <TextField
+              name="username"
+              label="Please Enter Your Username Here"
+              type="text"
+              variant="outlined"
+              required
+              fullWidth
+              onChange={handleChange}
+              autoFocus
+            />
+          </div>
+
+          <div className="register-field">
+            <TextField
+              name="password"
+              label="Please Enter Your Password Here"
+              type="text"
+              variant="outlined"
+              required
+              fullWidth
+              onChange={handleChange}
+              autoFocus
+            />
+          </div>
+
+          <div className="register-field">
+            <TextField
+              name="firstName"
+              label="Please Enter Your First Name Here"
+              type="text"
+              variant="outlined"
+              required
+              fullWidth
+              onChange={handleChange}
+              autoFocus
+            />
+          </div>
+
+          <div className="register-field">
+            <TextField
+              name="lastName"
+              label="Please Enter Your Last Name Here"
+              type="text"
+              variant="outlined"
+              required
+              fullWidth
+              onChange={handleChange}
+              autoFocus
+            />
+          </div>
+
+          <div className="register-field">
+            <TextField
+              name="email"
+              label="Please Enter Your Email Here"
+              type="text"
+              variant="outlined"
+              required
+              fullWidth
+              onChange={handleChange}
+              autoFocus
+            />
+          </div>
+
+          <div className="register-field">
+            <TextField
+              name="phoneNumber"
+              label="Please Enter Your Phone Number Here"
+              type="text"
+              variant="outlined"
+              fullWidth
+              onChange={handleChange}
+              autoFocus
+            />
+          </div>
+
+          {error && <div className="err-msg">{error}</div>}
+
+          <Button type="submit" variant="contained" color="primary">
+            Submit
+          </Button>
+        </form>
+
+        <div className="back">
+          <Button
+            variant="contained"
+            color="secondary"
+            component={Link}
+            to="/login"
+          >
+            Back to Login
+          </Button>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default Register;
