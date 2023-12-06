f557721ef0088c234d27cbd702ff65e038f4d907
Markus Valvur <mvalvur@asu.edu>
2023-12-05 22:35:39 -0800

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mysql = require("mysql2");
+const mysql = require("mysql");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
